e1b3e9912f6d877e64e973c2d9f14a778173794a
illia.serbin <illia.serbin@tangramltd.com>
2023-09-22 13:29:47 +0300

    Lint

--- src/types/hooks.ts
+++ src/types/hooks.ts
@@ -1,12 +1,14 @@
 import type {AxiosError} from 'axios';
 
 export interface IHooks {
-  /* Getter to receive current refresh token
+  /*
+   * Getter to receive current refresh token
    * @return {string | false}
    */
   getRefreshToken(): string | false;
 
-  /* Request on backend to receivce new token and refresh token
+  /*
+   * Request on backend to receivce new token and refresh token
    * @param {string} refreshToken
    * @return { Promise<{ token: string, refresh_token: string } | false> }
    */
@@ -14,13 +16,15 @@ export interface IHooks {
     refreshToken: string
   ): Promise<false | {token: string; refresh_token: string}>;
 
-  /* Hook to update your localStorage, vuex, pinia, etc. with new tokens
+  /*
+   * Hook to update your localStorage, vuex, pinia, etc. with new tokens
    * @param {{token: string, refresh_token: string}} newTokens
    * @return {void}
    */
   updateTokens(newTokens: {token: string; refresh_token: string}): void;
 
-  /* Hook to perform logout logic, including redirect to login page
+  /*
+   * Hook to perform logout logic, including redirect to login page
    * @param {AxiosError} error
    * @return {void}
    */
